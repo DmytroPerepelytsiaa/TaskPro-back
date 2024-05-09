9324f29b8d946c1a50d51fa58c84c2967eb6ce17
VIPon4ik <umniyd@gmail.com>
2024-05-10 00:19:02 +0300

    made guard and put logic to service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,30 +1,33 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
-import { JwtService } from '@nestjs/jwt';
+import { Controller, Post, Body, Get, UseGuards, Req } from '@nestjs/common';
+import { AuthGuard } from 'src/common/guards/auth.guard';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { IsUserExistPipe } from './pipes/is-user-exist.pipe';
 import { LoginUserDto } from './dto/login-user.dto';
 import { ValidateLoginPayloadPipe } from './pipes/validate-login-payload.pipe';
+import { UserEntity } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(
     private readonly usersService: UsersService,
-    private readonly jwtService: JwtService,
   ) {}
 
   @Post('register')
   async create(@Body(IsUserExistPipe) body: CreateUserDto): Promise<{ token: string }> {
-    body.password = await bcrypt.hash(body.password, 10);
-    const token = await this.jwtService.signAsync({ email: body.email });
     await this.usersService.createUser(body);
-    return { token };
+    return { token: await this.usersService.generateToken(body.email) };
   }
 
   @Post('login')
   async login(@Body(ValidateLoginPayloadPipe) body: LoginUserDto): Promise<{ token: string }> {
-    const token = await this.jwtService.signAsync({ email: body.email });
-    return { token };
+    return { token: await this.usersService.generateToken(body.email) };
+  }
+
+  @Get('me')
+  @UseGuards(AuthGuard)
+  getCurrentUser(@Req() req): Promise<UserEntity> {
+    const email = req.user.email;
+    return this.usersService.getUserByEmail(email);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 import { UserEntity } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -8,9 +10,19 @@ import { CreateUserDto } from './dto/create-user.dto';
 export class UsersService {
   constructor (
     @InjectRepository(UserEntity) private userRepository: Repository<UserEntity>,
+    private readonly jwtService: JwtService,
   ) {}
 
-  createUser(body: CreateUserDto): Promise<UserEntity> {
+  async createUser(body: CreateUserDto): Promise<UserEntity> {
+    body.password = await bcrypt.hash(body.password, 10);
     return this.userRepository.save({ ...body });
+  }
+
+  getUserByEmail(email: string): Promise<UserEntity> {
+    return this.userRepository.findOneBy({ email });
+  }
+
+  generateToken(email: string): Promise<string> {
+    return this.jwtService.signAsync({ email });
   }
 }
